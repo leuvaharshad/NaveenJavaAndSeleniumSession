f4df47e54d9d36b94e9fce2d8f8667aa8025c328
leuvaharshad <leuva.harshad@gmail.com>
2019-11-08 23:00:33 +0530

    changes done in ActionSendkey file and new file created under Javasession package

--- src/main/java/ActionClassConcept/ActionSendkey.java
+++ src/main/java/ActionClassConcept/ActionSendkey.java
@@ -17,6 +17,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class ActionSendkey {
 
 	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Testing git");
 
 			WebDriverManager.chromedriver().setup();
 			WebDriver driver= new ChromeDriver();
---
+++ src/main/java/JavaSession/GitClass.java
@@ -0,0 +1,10 @@
+package JavaSession;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is for git testing");
+
+	}
+
+}
